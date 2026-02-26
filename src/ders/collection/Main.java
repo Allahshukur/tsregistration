849bf25f2f8039cd10dac0b04e643ad91fffc5a0
Allahshukur <agazade.424@gmail.com>
2026-02-26 21:03:28 +0400

    chack

--- src/ders/collection/Main.java
+++ src/ders/collection/Main.java
@@ -1,29 +1,13 @@
 package ders.collection;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MyLinkedList<String> cars = new MyLinkedList<>();
 
-        cars.add("BMW");
-        cars.add("Hyundai");
-        cars.add("Toyota");
-        cars.addLast("Lexus");
-        cars.addFirst("Mercedes");
-        cars.add("sdsdsd");
-        cars.add("dddsdsd");
-
-        System.out.println(cars.get(0));
-        System.out.println(cars.get(1));
-        System.out.println(cars.get(2));
-        System.out.println(cars.get(3));
-        System.out.println(cars.get(4));
-        System.out.println(cars.get(5));
-        System.out.println(cars.get(6));
 
     }
 }
